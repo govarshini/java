6f2ebbe5a580513445dc8bf0945f671af82b39f1
govarshini <govarshiniarumugam@gmail.com>
2017-06-12 15:00:05 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class leap
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%4==0)
+		System.out.println("leap yaer");
+		else
+		System.out.println("not");
+	}
+}
